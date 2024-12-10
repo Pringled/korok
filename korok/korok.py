0c86f2498a57208fc60d853e3bf6c60d2c9fb29d
bhavnicksm <bhavnicksm@gmail.com>
2024-12-11 01:30:10 +0530

    feat: Implement hybrid search functionality with BM25S and Ranx integration
    - Added support for optional BM25S model in the Pipeline class.
    - Introduced methods for checking and importing hybrid components.
    - Implemented result fusion between BM25S and vicinity models.
    - Updated the fit method to accommodate hybrid search option.

--- korok/korok.py
+++ korok/korok.py
@@ -1,9 +1,17 @@
 from __future__ import annotations
 
-from typing import Any
+from typing import Any, TYPE_CHECKING
+
+import importlib
 
 from model2vec import StaticModel
 from vicinity import Backend, Metric, Vicinity
+
+# We need to import these classes to check if they are installed
+if TYPE_CHECKING:
+    import bm25s
+    from bm25s import BM25S
+    import ranx
 
 
 class Pipeline:
@@ -11,21 +19,41 @@ class Pipeline:
         self,
         encoder: StaticModel,
         vicinity: Vicinity,
+        bm25s: "BM25S" | None = None
     ) -> None:
         """
         Initialize a Pipeline instance.
 
         :param encoder: The encoder used to encode the items.
         :param vicinity: The vicinity object used to find nearest neighbors.
+        :param bm25s: The BM25S object used for keyword search.
         """
         self.encoder = encoder
         self.vicinity = vicinity
+
+        # Keeping the hybrid components separate from the main pipeline
+        # and importing them only when needed
+        if bm25s is not None:
+            self._check_and_import_hybrid_components()
+            self.bm25 = bm25s
+            self.rrf = ranx.RRF()
+        
+
+    # To check if the hybrid components are installed
+    def _check_and_import_hybrid_components(self) -> bool:
+        try:
+            import bm25s
+            import ranx
+            return True
+        except ImportError:
+            raise ImportError("hybrid is not installed. Please install it using `pip install korok[hybrid]`.")
 
     @classmethod
     def fit(
         cls,
         texts: list[str],
         encoder: StaticModel,
+        hybrid: bool = False,
         **kwargs: Any,
     ) -> Pipeline:
         """
@@ -44,7 +72,45 @@ class Pipeline:
             metric=Metric.COSINE,
             **kwargs,
         )
+        # If hybrid is enabled, we need to fit the BM25S model
+        if hybrid:
+            bm25 = bm25s.BM25S()
+            corpus_tokens = bm25s.tokenize(texts, stopwords="en")
+            bm25.index(corpus_tokens)
+            return cls(encoder=encoder, vicinity=vicinity, bm25s=bm25)
+        
         return cls(encoder=encoder, vicinity=vicinity)
+    
+    def _fuse_results(self, 
+                      texts: list[str],
+                      results: list[list[tuple[str, float]]],
+                      bm25_results: list[list[tuple[str, float]]]) -> list[list[tuple[str, float]]]:
+        """Fuse the results from the BM25S model and the vicinity model."""
+        # Convert the results to a ranx Run objects
+        bm25_run_dict = {
+            "query_id": [i for i in range(len(texts))],
+            "doc_id": [doc_id for doc_id in bm25_results.keys()],
+            "score": [score for score in bm25_results.values()],
+        }
+        bm25_run = ranx.Run(bm25_run_dict, name="bm25")
+
+        vicinity_run_dict = {
+            "query_id": [i for i in range(len(texts))],
+            "doc_id": [doc_id for doc_id in results.keys()],
+            "score": [score for score in results.values()],
+        }
+        vicinity_run = ranx.Run(vicinity_run_dict, name="vicinity")
+
+        # Fuse the results
+        results = ranx.fuse(runs=[bm25_run, vicinity_run],
+                            method="rrf",
+                            norm="rank"
+                            ).to_dict()
+        results = [
+            [(doc_id, score) for doc_id, score in run.items()]
+            for run in results.values()
+        ]
+        return results
 
     def query(
         self,
@@ -61,4 +127,13 @@ class Pipeline:
         """
         vectors = self.encoder.encode(texts, show_progressbar=True)
         results = self.vicinity.query(vectors, k)
+
+        # If the BM25S model is enabled, we need to fuse the results
+        if self.bm25 is not None:
+            query_tokens = self.bm25s.tokenize(texts)
+            bm25_results = self.bm25.retrieve(query_tokens, k)
+            results = self._fuse_results(texts, results, bm25_results)
+
         return results
+
+
